6f8dbb7c2ac018413e8ca366a773f222dc9d7249
Devin Conway <devinthomas1208@gmail.com>
2022-07-11 10:56:48 -0400

    content: added title tag

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,6 +5,7 @@
 
 <svelte:head>
 	<script src="https://kit.fontawesome.com/b6dbfc57a8.js" crossorigin="anonymous"></script>
+	<title>Devin Conway &mdash; Portfolio</title>
 </svelte:head>
 
 <div class="p-8 max-w-6xl mx-auto">
