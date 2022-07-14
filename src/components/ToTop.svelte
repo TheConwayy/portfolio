3b0e70f24515ac98cb6496807155c0cd3dc6377d
Devin Conway <devinthomas1208@gmail.com>
2022-07-13 22:24:27 -0400

    update: added a to-top button

--- src/components/ToTop.svelte
+++ src/components/ToTop.svelte
@@ -1,17 +1,28 @@
-<button>
-    <i class="fa-solid fa-arrow-up"></i>
+<script>
+	let element;
+</script>
+
+<div bind:this={element}>
+	<slot />
+</div>
+
+<button
+	on:click={() => {
+		element.scrollIntoView();
+	}}
+>
+	<i class="fa-solid fa-arrow-up" />
 </button>
 
 <style>
+	button {
+		background: #d1d1d1;
+		padding: 10px 20px 10px 20px;
+		border-radius: 50%;
+		font-size: 1.75rem;
 
-    button {
-        background: #d1d1d1;
-        padding: 15px;
-        border-radius: 50%;
-    }
-
-    i {
-        color: #121212
-    }
-
+		position: fixed;
+		bottom: 40px;
+		right: 40px;
+	}
 </style>
